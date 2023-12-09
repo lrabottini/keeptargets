793c23cb76ef73d8fde42430b9efeb806f473266
lrabottini <lrabottini@gmail.com>
2023-12-09 09:18:56 -0300

    update

--- back-end/centro-custo/src/routes/list-all.js
+++ back-end/centro-custo/src/routes/list-all.js
@@ -28,7 +28,6 @@ router.get('/centrocusto/all/:org', async (req, res) => {
 
         const estruturaAplanada = aplanarEstrutura(centrocusto);
         
-        console.log('realizou consulta')
         res.send(estruturaAplanada)
     } catch (e) {
         const message = new ExecutionMessage(
