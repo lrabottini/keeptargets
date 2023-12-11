26270675ac42c640a79a4121d92c5d9c84118618
lrabottini <lrabottini@gmail.com>
2023-12-11 10:08:16 -0300

    alteração rota update, ms centro de custo

--- back-end/centro-custo/src/routes/list-all.js
+++ back-end/centro-custo/src/routes/list-all.js
@@ -4,37 +4,9 @@ import { CentroCusto } from '../models/centro-custo.js'
 
 const router = express.Router()
 
-// function aplanarEstrutura(hierarquia) {
-//     const flatHierarchy = [];
-//     let ordem = 100000
-    
-//     function aplanar(item, depth, ordem) {
-//         const { _id, centrocusto_parent, centrocusto_descr, centrocusto_cod, centrocusto_org, children } = item;
-//         const centrocusto_hasChildren = children.length > 0
-//         //const index = ordem + depth
-        
-//         flatHierarchy.push({ _id, centrocusto_parent, centrocusto_descr, centrocusto_cod, centrocusto_org, centrocusto_hasChildren, depth, ordem });
-        
-//         ordem = ordem + 1
-            
-//         if (children && children.length > 0) {
-
-//             children.forEach(child => aplanar(child, depth + 1, ordem));
-//         }
-//     }
-
-//     hierarquia.forEach(item => {
-//         aplanar(item, 0, ordem)
-
-//         ordem = ordem + 100000
-//     });
-//     return flatHierarchy;
-// }
-
 function aplanarEstrutura(hierarquia) {
     const flatHierarchy = [];
-    const prefixStack = [];
-
+    
     function aplanar(item, prefix, level) {
         const { _id, children, ...rest } = item;
 
