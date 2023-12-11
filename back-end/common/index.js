c7ce39bbfa2a3df1b7857a709407bef5cba8c3d6
lrabottini <lrabottini@gmail.com>
2023-12-11 10:46:05 -0300

    updates

--- back-end/common/index.js
+++ back-end/common/index.js
@@ -4,6 +4,7 @@ export * from './src/events/message-header.js'
 export * from './src/events/message-tools.js'
 
 export * from './src/messaging/execution-message.js'
+export * from './src/messaging/error-message.js'
 export * from './src/messaging/execution-types.js'
 export * from './src/messaging/execution-status.js'
 export * from './src/messaging/enum.js'
