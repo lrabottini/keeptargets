6d4e08728a6d785e7d7af13855cf0781ba8b5509
lrabottini <lrabottini@gmail.com>
2023-12-13 16:03:02 -0300

    ms situacao

---
+++ back-end/situacao/src/routes/list-all.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import { ExecutionMessage, ExecutionTypes, ExecutionStatus, MessageLevel } from '@keeptargets/common'
+import { Situacao } from '../models/situacao.js'
+
+const router = express.Router()
+
+const filter = (filtro) => {
+    let filter = {}
+
+    if (filtro.hasOwnProperty('objeto')) {
+        filter = {
+            situacao_objeto: `${filtro.objeto}`
+        }
+    }
+    return filter
+}
+
+
+router.get('/situacao/?', async (req, res) => {
+    try {
+        const situacao = await Situacao.find(filter(req.query))
+
+        res.send(situacao)
+    } catch (e) {
+        const error = [{
+            type: e.name,
+            value: '',
+            msg: e.message,
+            path: e.stack,
+            location: ''
+        }]
+
+        const message = new ExecutionMessage(
+            MessageLevel.LEVEL_ERROR,
+            ExecutionStatus.ERROR,
+            ExecutionTypes.LIST,
+            'Erro ao buscar situações.',
+            req.params,
+            error 
+        )
+        res.send(message)
+    }
+})
+
+export { router as listAll }
