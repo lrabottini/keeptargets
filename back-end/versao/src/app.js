6cb20aa37e5b239739fb44251944122f315141d8
lrabottini <lrabottini@gmail.com>
2023-12-18 12:50:19 -0300

    update ms versao, remoção da rota listOne

--- back-end/versao/src/app.js
+++ back-end/versao/src/app.js
@@ -2,7 +2,6 @@ import express from 'express';
 
 import { createVersao } from './routes/create.js'
 import { deleteVersao } from './routes/delete.js'
-import { listOneVersao } from './routes/list-one.js'
 import { listAllVersao } from './routes/list-all.js'
 import { updateVersao } from './routes/update.js'
 
@@ -13,7 +12,6 @@ app.use(express.json());
 
 app.set('trust proxy', true);
 
-app.use(listOneVersao)
 app.use(listAllVersao)
 app.use(createVersao)
 app.use(deleteVersao)
