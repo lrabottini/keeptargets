d49970105c2fadf0b666a8d0e99975c47bc25f79
lrabottini <lrabottini@gmail.com>
2023-12-19 15:33:26 -0300

    update model, ms versão update rota listAll, ms versão

--- back-end/versao/src/models/versao.js
+++ back-end/versao/src/models/versao.js
@@ -38,6 +38,72 @@ const versaoSchema = new mongoose.Schema({
     }
 })
 
+versaoSchema.statics.listVersao = async function (org, ciclo) {
+    const response = await this.aggregate([
+        {
+            $match: {
+                versao_org : new mongoose.Types.ObjectId(org),
+                versao_ciclo: new mongoose.Types.ObjectId(ciclo)
+            }
+        },
+        /** Faz o lookup para trazer a estrutura */
+        {
+            $lookup: {
+                from: 'estrutura',
+                localField: 'versao_estrutura',
+                foreignField: '_id',
+                pipeline:[
+                    {
+                        $project: {
+                            'estrutura_descr': 1,
+                        }
+                    }
+                ],
+                as: 'estrutura',
+            }
+        }
+        ,{
+            $unwind: {
+                path: '$estrutura',
+                preserveNullAndEmptyArrays: true
+            }
+        }
+        /** Faz o lookup para trazer o responsável principal */
+        ,{
+            $lookup: {
+                from: 'usuario',
+                localField: 'versao_responsavel',
+                foreignField: '_id',
+                pipeline:[
+                    {
+                        $project: {
+                            'usuario_nomecompleto': {
+                                $concat: ["$usuario_nome", " ", "$usuario_sobrenome"]
+                            }
+                        }
+                    }
+                ],
+                as: 'responsavel',
+            }
+        }
+        ,{
+            $unwind: {
+                path: '$responsavel',
+                preserveNullAndEmptyArrays: true
+            }
+        }
+        ,{
+            $project: {
+                __v: 0,
+                createdAt: 0,
+                lastModified: 0
+                }
+        }
+    ]).exec()
+
+    return response
+}
+
 const Versao = mongoose.model('Versao', versaoSchema)
 
 export { Versao }
--- back-end/versao/src/routes/list-all.js
+++ back-end/versao/src/routes/list-all.js
@@ -5,15 +5,15 @@ import mongoose from 'mongoose'
 
 const router = express.Router()
 
-router.get('/versao/?', async (req, res) => {
+router.get('/versao/:org/:ciclo', async (req, res) => {
     try {
-        const filter = {}
-        for (const key in req.query){
-            if (req.query[key] !== '')
-                filter[`versao_${key}`] = new mongoose.Types.ObjectId(req.query[key])
-        }
+        // const filter = {}
+        // for (const key in req.query){
+        //     if (req.query[key] !== '')
+        //         filter[`versao_${key}`] = new mongoose.Types.ObjectId(req.query[key])
+        // }
 
-        const versao = await Versao.find(filter)
+        const versao = await Versao.listVersao(req.params.org, req.params.ciclo)
 
         res.send(versao)
     } catch (e) {
@@ -28,7 +28,7 @@ router.get('/versao/?', async (req, res) => {
         const message = new ExecutionMessage(
             MessageLevel.LEVEL_ERROR,
             ExecutionStatus.ERROR,
-            ExecutionTypes.DELETE,
+            ExecutionTypes.LIST,
             'Não foi possível retornar versões.',
             req.params,
             error 
