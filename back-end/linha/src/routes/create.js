e75c53ab20c33ad3407e5ce6fd9c1b7b49978560
lrabottini <lrabottini@gmail.com>
2023-12-20 21:30:01 -0300

    update rota create, ms linha

--- back-end/linha/src/routes/create.js
+++ back-end/linha/src/routes/create.js
@@ -14,13 +14,15 @@ router.post('/linha', async (req, res) => {
         if (result.isEmpty()){
             const linha = new Linha()
 
-            linha.linha_versao = req.body.versao,
-            linha.linha_classificacao = req.body.classificacao,
-            linha.linha_centro_de_custo = req.body.cc,
-            linha.linha_tipo_de_despesa = req.body.tipo_de_despesa,
-            linha.linha_estrutura = req.body.estrutura,
-            linha.linha_proprietario = req.body.proprietario,
-            linha.linha_fornecedor = req.body.fornecedor,
+            linha.linha_org = new mongoose.Types.ObjectId(req.body.org)
+            linha.linha_ciclo = new mongoose.Types.ObjectId(req.body.ciclo)
+            linha.linha_versao = new mongoose.Types.ObjectId(req.body.versao)
+            linha.linha_classificacao = new mongoose.Types.ObjectId(req.body.classificacao),
+            linha.linha_centro_de_custo = new mongoose.Types.ObjectId(req.body.cc),
+            linha.linha_tipo_de_despesa = new mongoose.Types.ObjectId(req.body.tipo_de_despesa),
+            linha.linha_estrutura = new mongoose.Types.ObjectId(req.body.estrutura),
+            linha.linha_proprietario = new mongoose.Types.ObjectId(req.body.proprietario),
+            linha.linha_fornecedor = new mongoose.Types.ObjectId(req.body.fornecedor),
             linha.linha_descricao = req.body.descricao,
             linha.linha_inicio_periodo = toFormattedDate(req.body.start),
             linha.linha_fim_periodo = toFormattedDate(req.body.end),
@@ -30,7 +32,7 @@ router.post('/linha', async (req, res) => {
             linha.linha_reajuste.reajuste_valor = req.body.reajuste_valor,
             linha.linha_valor_final.valor_final_perc = req.body.valor_final_perc,
             linha.linha_valor_final.valor_final_valor = req.body.valor_final_valor,
-            linha.linha_etapa = req.body.etapa,
+            linha.linha_etapa = new mongoose.Types.ObjectId(req.body.etapa)
             linha.linha_observacao = req.body.obs
 
             await linha.save()
