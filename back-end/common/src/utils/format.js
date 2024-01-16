875dbb16fdd3d48bb4962645f5ecb8ccc037f1ca
lrabottini <lrabottini@gmail.com>
2024-01-16 13:18:37 -0300

    updates

--- back-end/common/src/utils/format.js
+++ back-end/common/src/utils/format.js
@@ -1,6 +1,14 @@
 const toFormattedDate = (date) => {
     let [d, m, y] = date.split(/[\/: ]/).map(v => parseInt(v))
-    return new Date(y, m - 1, d)
+
+    console.log(d)
+    console.log(m)
+    console.log(y)
+
+    const newDate = new Date(y, m - 1, d)
+    
+    console.log(newDate)
+    return newDate
 }
 
 export { toFormattedDate }
