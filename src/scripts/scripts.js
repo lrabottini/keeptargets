cc5dafd17fc2ea2c55b3f66803fd323b58604fe1
Luciano <lrabottini@gmail.com>
2024-09-27 12:32:57 -0300

    ajustes nos scripts

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,4 +1,4 @@
-function calculaValorFinalMonetario(valor_final) {
+function calculaValorFinalMonetario() {
     function parseCurrency(value) {
         if (!value) {
             return 0;
@@ -7,42 +7,65 @@ function calculaValorFinalMonetario(valor_final) {
         return Math.round(parseFloat(value.replace('R$ ', '').replace(/\./g, '').replace(',', '.')) * 100) || 0;
     }
 
-    // Função para formatar o valor como Real brasileiro (R$)
-    function formatToBRL(value) {
-        return (value / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    }
-
-    var target = document.getElementById('input_valor_final_monetario');
     let elements = document.querySelectorAll("[id^='input_previsto_monetario']");
+    let total = 0;
 
     if (elements.length > 0) {
-        let total = 0;
-    
+   
         // Soma os valores de cada campo input em centavos
         elements.forEach(function (element) {
             total += parseCurrency(element.value);
         });
-
-        // Formata o valor total e exibe no campo de valor final
-        target.value = formatToBRL(total);
-    } else {
-        // Se não houver elementos, define o valor como R$ 0,00
-        target.value = formatToBRL(0);
     }
+
+    return total/100
 }
 
-function retorna_valor_previsto_monetario(id){
-    const previsto_monetario = document.getElementById(`input_previsto_monetario_${id}`)
+bubble_fn_calculavalorfinalmonetario(calculaValorFinalMonetario())
 
-    if (previsto_monetario) {
-            if (previsto_monetario.value === null){
-                return 0
-            } else {
-                return parseFloat(previsto_monetario.value.replace(/[^0-9,-]+/g, '').replace(',', '.')).toFixed(2)
-            }
-    } else {
-        return 0;
+function totalizaValores() {
+    function parseCurrency(value) {
+        if (!value) {
+            return 0;
+        }
+        // Remove "R$ ", pontos de milhar e converte para número em centavos
+        return Math.round(parseFloat(value.replace('R$ ', '').replace(/\./g, '').replace(',', '.')) * 100) || 0;
     }
+
+    let allPrevistoValor = document.querySelectorAll("[id^='input_previsto_monetario']");
+    let totalPrevistoValor = 0;
+
+    if (allPrevistoValor.length > 0) {
+   
+        // Soma os valores de cada campo input em centavos
+        allPrevistoValor.forEach(function (element) {
+            totalPrevistoValor += parseCurrency(element.value);
+        });
+    }
+
+    let allRealizadoValor = document.querySelectorAll("[id^='input_realizado_valor']");
+    let totalRealizadoValor = 0;
+
+    if (allRealizadoValor.length > 0) {
+   
+        // Soma os valores de cada campo input em centavos
+        allRealizadoValor.forEach(function (element) {
+            totalRealizadoValor += parseCurrency(element.value);
+        });
+    }
+    
+    let allRealizadoUnidade = document.querySelectorAll("[id^='input_realizado_unidade']");
+    let totalRealizadoUnidade = 0;
+
+    if (allRealizadoUnidade.length > 0) {
+   
+        // Soma os valores de cada campo input em centavos
+        allRealizadoUnidade.forEach(function (element) {
+            totalRealizadoUnidade += parseFloat(element.value);
+        });
+    }
+    
+    return [totalPrevistoValor/100, totalRealizadoValor/100, totalRealizadoUnidade]
 }
 
 function get_index(valor){
@@ -72,6 +95,92 @@ function listaValorPrevistoUnidade() {
         return []
     }
 }
+
+function calculaValorFinalMonetario() {
+    function parseCurrency(value) {
+        if (!value) {
+            return 0;
+        }
+        // Remove "R$ ", pontos de milhar e converte para número em centavos
+        return Math.round(parseFloat(value.replace('R$ ', '').replace(/\./g, '').replace(',', '.')) * 100) || 0;
+    }
+
+    let elements = document.querySelectorAll("[id^='input_previsto_valor']");
+    let total = 0;
+
+    if (elements.length > 0) {
+   
+        // Soma os valores de cada campo input em centavos
+        elements.forEach(function (element) {
+            total += parseCurrency(element.value);
+        });
+    }
+
+    return total/100
+}
+
+bubble_fn_calculavalorfinalmonetario(calculaValorFinalMonetario())
+
+// Função para retornar os valores previstos e realizados, por distribuição
+function retornaValores(id){
+    function parseCurrency(value) {
+        if (!value) {
+            return 0;
+        }
+        // Remove "R$ ", pontos de milhar e converte para número em centavos
+        return Math.round(parseFloat(value.replace('R$ ', '').replace(/\./g, '').replace(',', '.')) * 100) || 0;
+    }
+
+    var result = []
+
+    const previstoValor = document.getElementById(`input_previsto_valor_${id}`)
+    if (previstoValor) {
+            if (previstoValor.value === null){
+                result.push(0)
+            } else {
+                result.push(parseCurrency(previstoValor.value)/100)
+            }
+    } else {
+        result.push(0)
+    }
+
+    const realizadoValor = document.getElementById(`input_realizado_valor_${id}`)
+    if (realizadoValor) {
+            if (realizadoValor.value === null){
+                result.push(0)
+            } else {
+                result.push(parseCurrency(realizadoValor.value)/100)
+            }
+    } else {
+        result.push(0)
+    }
+
+    const previstoUnidade = document.getElementById(`input_previsto_unidade_${id}`)
+    if (previstoUnidade) {
+            if (previstoUnidade.value === null){
+                result.push(0)
+            } else {
+                result.push(parseFloat(previstoUnidade.value))
+            }
+    } else {
+        result.push(0)
+    }
+
+    const realizadoUnidade = document.getElementById(`input_previsto_unidade_${id}`)
+    if (realizadoUnidade) {
+            if (realizadoUnidade.value === null){
+                result.push(0)
+            } else {
+                result.push(parseFloat(realizadoUnidade.value))
+            }
+    } else {
+        result.push(0)
+    }
+
+    return result
+}
+
+retornaPrevistoValor()
 
 // Script Máscara R$
 document.addEventListener('input', function(event) {
@@ -105,10 +214,11 @@ document.addEventListener('input', function(event) {
     let checkExist = setInterval(function() {
         var input1 = document.getElementById('input_valor');
         var input2 = document.getElementById('input_valor_real');
-        var input3 = document.querySelectorAll('[id^=input_previsto_monetario]'); // Array de elementos para input3
+        var input3 = document.querySelectorAll('[id^=input_previsto_valor]');
         var input4 = document.getElementById('input_reajuste_monetario')
+        var input5 = document.querySelectorAll('[id^=input_realizado_valor]');
         
-        if (input1 || input2 || input3.length > 0 || input4) {
+        if (input1 || input2 || input3.length > 0 || input4 || input5.length > 0) {
             // Aplica eventos de formatação nos campos
             [input1, input2, input4].forEach(input => {
                 if (input) {
@@ -123,6 +233,16 @@ document.addEventListener('input', function(event) {
 
             // Para input3 (array), aplica a formatação em cada elemento
             input3.forEach(input => {
+                input.addEventListener('input', function() {
+                    applyCurrencyFormatting(input);
+                });
+                input.addEventListener('keydown', function(event) {
+                    restrictInputToNumbers(event);
+                });
+            });
+
+            // Para input3 (array), aplica a formatação em cada elemento
+            input5.forEach(input => {
                 input.addEventListener('input', function() {
                     applyCurrencyFormatting(input);
                 });
